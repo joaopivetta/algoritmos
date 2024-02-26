1f00266a3c560c9a0cf6a7af1b5f9f9a435171f6
joaopivetta <142848416+joaopivetta@users.noreply.github.com>
2024-02-26 19:49:00 -0300

    Delete aula09 directory

--- aula09/exercicio2.py
+++
@@ -1,9 +0,0 @@
-lista = []
-
-for i in range(0, 5):
-    lista.append(int(input(f"Digite o número {i+1}: ")))
-
-    maior = max(lista)
-    pos = lista.index(max(lista))
-
-print(f"O maior é: {maior}, e a sua posição é: {pos}")
