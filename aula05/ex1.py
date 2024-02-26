c8756bace55f70d0b003b60fcf9c2fb20d88d837
joaopivetta <142848416+joaopivetta@users.noreply.github.com>
2024-02-26 19:49:23 -0300

    Delete aula05 directory

--- aula05/ex1.py
+++
@@ -1,7 +0,0 @@
-a = float(input("Entre com o valor a: "))
-b = float(input("Entre com o valor de b: "))
-
-if a < b:
-    print(f"O menor número é o {a}")
-else:
-    print(f"O menor número é {b}")
--- aula05/exercicio2.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Entre com o valor a: "))
-b = int(input("Entre com o valor de b: "))
-
-if b != 0:
-    divisao = a/b
-    print("O valor da divisão é: ", divisao)
-else:
-    print("Não posso dividir por 0")
