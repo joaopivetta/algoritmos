cf23edd351045668169fd3cad371ba712303a592
joaopivetta <142848416+joaopivetta@users.noreply.github.com>
2024-02-26 19:48:06 -0300

    Delete Aula06 directory

--- Aula06/exercicio1.py
+++
@@ -1,6 +0,0 @@
-x = int(input("Digite um número: "))
-
-if x % 5 == 0 and x % 3 == 0 :
-    print("Esse número é divisível por 5 e por 3!")
-else:
-    print("Esse número não é divisível por 5 e por 3")
--- Aula06/exercicio3.py
+++
@@ -1,9 +0,0 @@
-xAposento = float(input("Entre com a primeira medida do aposento: "))
-yAposento = float(input("Entre com a segunda medida do aposento: "))
-litrosTinta = float(input("Entre com a quantidade em litros da lata de tinta: "))
-
-medidaAposento = (( xAposento * 2.8) + (yAposento * 2.8) * 2) - (2.1 *0.8)
-
-qtdLatas = medidaAposento / 3
-
-print(f"A quantidade de latas necessárias é {round(qtdLatas):2.0f}")
