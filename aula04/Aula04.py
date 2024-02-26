1284fbc4f6b1dd07f5c3de653faf5d5a9d573cfb
joaopivetta <142848416+joaopivetta@users.noreply.github.com>
2024-02-26 19:49:16 -0300

    Delete aula04 directory

--- aula04/Aula04.py
+++
@@ -1,4 +0,0 @@
-base = int(input("entre com a base: "))
-alt = int(input("entre com a altura: "))
-area = base * alt
-print("base= ,", base, "altura= ,", alt, "área= ,", area)
--- aula04/aula04_ex2.py
+++
@@ -1,3 +0,0 @@
-tempC = float(input("Entre com a temperatura em Celsius: "))
-tempF = tempC * 1.8 + 32
-print('Temperatura em Fº ', tempF, 'Fº')
