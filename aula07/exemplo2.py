2e7c6d1dd197e1966d3735bfbf27206ad07ec6bf
joaopivetta <142848416+joaopivetta@users.noreply.github.com>
2024-02-26 19:49:30 -0300

    Delete aula07 directory

--- aula07/exemplo2.py
+++
@@ -1,7 +0,0 @@
-soma = 0
-for i in range(1, 11):
-    n = int(input(f"Entre com o {i}a idade: "))
-    soma = soma + n
-media = soma / 10
-
-print(f"A média é {media:5.2f}")
--- aula07/exemplo3.py
+++
@@ -1,8 +0,0 @@
-soma = 0
-qtd = int(input("Entre com a quantidade de idades: "))
-
-for i in range(1, qtd + 1):
-    n = int(input(f"Entre com a {i}a idade: "))
-    soma = soma + n
-media = soma / qtd
-print(f" A média é: {media}")
